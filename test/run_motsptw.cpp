858b4901d3d5dbd3ce99715b0391fb81946360f7
wyc23 <wuyancheng@sjtu.edu.cn>
2024-04-03 03:41:15 +0800

    add: motsptw-runner

---
+++ test/run_motsptw.cpp
@@ -0,0 +1,42 @@
+#include "search_motsptw.hpp"
+#include "data_loader.hpp"
+#include <iostream>
+#include <string>
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "invalid input" << std::endl;
+        return -1;
+    }
+
+    rzq::basic::SparseGraph g;
+    std::string fn = argv[1];
+
+    rzq::basic::DataLoader dl(fn);
+    dl.Load();
+    dl.CreateGraph(g);
+
+    long vo = 0;
+    long vd = dl.GetVd();
+
+    rzq::search::MOTSPTWResult res;
+    std::vector<std::pair<double, double>> tw = dl.GetTw();
+    std::vector<double> st = dl.GetSt();
+    RunMOTSPTW(&g, tw, st, vo, vd, &res);
+
+    // print paths, times and costs
+    std::cout << "---- reprint solutions for more clarity:" << std::endl;
+    for (auto iter : res.paths) {
+        long k = iter.first; // id of a Pareto-optipmal solution
+        // path nodes
+        std::cout << " path nodes = ";
+        for (auto xx : res.paths[k])
+        {
+            std::cout << xx << ", ";
+        }
+        std::cout << std::endl;
+        // cost
+        std::cout << " cost = " << res.costs[k] << std::endl;
+    }
+    return 0;
+}
